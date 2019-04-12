983f402e6e1c6ec55d29908fc358bda9b4785a87
Rommel Rico <me@rommelrico.com>
2019-04-11 20:25:29 -0700

    feat(API): Initial implementation

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const url = 'http://127.0.0.1:8000/api/v1/notes/';
+
+export const fetchNotes = async () => {
+    return fetch(url, {}).then(response => response.json()).then(data => data);
+};
+
+export const fetchNote = (id) => {
+    return { 'id': 123, 'title': 'Hello', 'content': 'Testing'};
+};
+
+export const addNote = (note) => {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+    }).then(response => response.json()).then(data => console.log(data));
+
+    return note;
+};
+
+export const updateNote = (note) => {
+    console.log("We're updating...");
+    console.log("Update note with id: " + note.id);
+};
